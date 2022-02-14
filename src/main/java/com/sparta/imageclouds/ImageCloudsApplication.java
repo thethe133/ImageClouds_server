e3fffd568b0af18d9007e5449c394fa97e2efcc5
thethe133@naver.com <thethe133@naver.com>
2022-02-15 00:53:18 +0900

    Initial commit

---
+++ src/main/java/com/sparta/imageclouds/ImageCloudsApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.imageclouds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageCloudsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageCloudsApplication.class, args);
+    }
+
+}
