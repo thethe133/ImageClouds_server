4df196893500deece20993d24da1ad29bf880484
thethe133@naver.com <thethe133@naver.com>
2022-02-16 02:34:05 +0900

    User, Article

--- src/main/java/com/sparta/imageclouds/repository/TagRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sparta.imageclouds.repository;
-
-import com.sparta.imageclouds.model.Tag;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TagRepository extends JpaRepository<Tag, Long> {
-    Tag findByKeyword(String keyword);
-}
